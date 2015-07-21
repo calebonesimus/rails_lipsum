2bc7ae8637a87fa0b4957c8e4e0bf16ef38ba1d9
Caleb Francis <calebonesimus@gmail.com>
2015-07-21 14:42:29 -0400

    Set default paragraph count to 1

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,10 +2,12 @@ class WelcomeController < ApplicationController
   def index
   end
 
+  # Personalize welcome message
   def show
     @name = params[:name].capitalize!
   end
 
+  # Display lorem ipsum with specifed type and quantity
   def lorem
     @num_paragraphs = params[:num_paragraphs].to_i
     @num_paragraphs = 1 if @num_paragraphs == 0
